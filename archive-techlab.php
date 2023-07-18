93bc0c802bbcfe6ee5f2c1fc8d029ad6561cc97d
Zhaniya110 <zmeyramova@gmail.com>
2023-07-18 22:20:24 +0600

    Commit

--- archive-techlab.php
+++ archive-techlab.php
@@ -1,37 +1,43 @@
 <?php get_header(); ?>
 
-    <div class="container pt-5" style="margin-top:100px; margin-bottom:100px;">
-      <div class="row product row-gap-5">
-    
+<div class="container pt-5" style="margin-top:100px; margin-bottom:100px;">
+    <div class="row product row-gap-5">
 
-              <!-- карточка курса/абонемента -->
-        <div class="col-md-4">
-        <?php if ( have_posts() ): ?>
-                <?php while( have_posts() ): ?>
+
+        <!-- карточка курса/абонемента -->
+      
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()): ?>
                     <?php the_post(); ?>
+                    <div class="col-md-4">
 
-
-            <div class="card">
-                <div class="card-body">
-                  <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                </div>
-                <hr />
-                <div class="card-body">
-                  <p><?php the_excerpt(); ?></p>
-                </div>
-             </div>
-             
-            <?php endwhile; ?>
-            <?php the_posts_pagination(); ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        </div>
+                        <hr />
+                        <div class="card-body">
+                            <p>
+                                <?php the_excerpt(); ?>
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                <?php endwhile; ?>
+                <?php the_posts_pagination(); ?>
             <?php else: ?>
-            <p><?php _e( 'No Blog Posts found', 'nd_dosth' ); ?></p>
+                <p>
+                    <?php _e('No Blog Posts found', 'nd_dosth'); ?>
+                </p>
             <?php endif; ?>
-                        
-         </div>
-               
 
-           
-          </div>
-      </div>
+       
 
-    <?php get_footer(); ?>
+
+
+    </div>
+</div>
+
+
+
+<?php get_footer(); ?>
