d50dedb6577ffd4b7598faa9fe2c2c176fb7053f
Zhaniya110 <zmeyramova@gmail.com>
2023-07-19 02:34:23 +0600

    Commit

---
+++ 404.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+
+    <div class="header p-3">
+        <div class="row">
+            <div class="col-md-4">
+               <a type="button" class="btn" href="arduino-archive.html">Назад</a>
+            </div>
+            <div class="col-md-4 text-center"><h1>TechLab</h1></div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+
+    <div class="container pt-5">
+      <div class="row product row-gap-5">
+        <!-- карточка курса/абонемента -->
+        <div class="col">
+         
+            <div class="card">
+              <div class="card-body">
+                <h5>404</h5>
+              </div>
+              <hr />
+              <div class="card-body">
+               <p>Упс не туда зашли :) </p>
+              </div>
+            </div>
+          
+        </div>
+
+        <div class="bg-white border-top fixed-bottom p-3">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4 text-center"><button type="button" class="signup-btn">ЗАПИСАТЬСЯ НА КУРС</button></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
